ee431c934a591bc0868792601f5844615f869af1
MumuNiMochii <69304294+MumuNiMochii@users.noreply.github.com>
2021-01-29 18:26:51 +0800

    Update: Ordinal Numbers/nth's String Emphasis
    This is my very first time since last time I had my hands on Python, to program and update the contents without relying from any tutorials, and this made me happy.
    
    It works correctly, meaning it shows if within the nth terms it's the 1st or 2nd and so on, still, I am afraid that beyond 20th term the string of succeeding ordinal numbers will still be displayed with 'th', e.g. 21th, 22th, and so on, which is wrong.
    
    I've yet to fix this one lol.

--- simple_mean_calculator.py
+++ simple_mean_calculator.py
@@ -1,11 +1,17 @@
-# Credits to Avilash Nethala, Apr 26, 2017
-# You can check him at:
-# https://medium.com/programminginpython-com/python-program-to-calculate-the-sum-of-elements-in-a-list-ed2b80db8268
-
 lst = []
-num = int(input("Input the number of items: -> "))
+num = int(input("Input the number of items: "))
 for n in range(num):
-    numbers = int(input("Enter the value: -> "))
+    # Arguments for Ordinal Numbers in a Set
+    ord = str(n+1)
+    if n == 0:
+        ord += "st"
+    elif n == 1:
+        ord += "nd"
+    elif n == 2:
+        ord += "rd"
+    else:
+        ord += "th"
+    numbers = int(input("Enter the "+ ord +" value: "))
     lst.append(numbers)
-print("The sum of the values is :", sum(lst))
+print("The sum of the values is: ", sum(lst))
 print("The mean is equal to: ", float(sum(lst) / num))
